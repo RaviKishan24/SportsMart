a02a234f54d2c2a0abc91398eb4c8f383763a99f
RaviKishan24 <Ravikishankumar71@gmail.com>
2025-04-05 22:08:56 +0530

    Fix: add missing passwordchange.css or correct path

--- client/src/Components/User/Passwordchange.jsx
+++ client/src/Components/User/Passwordchange.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
-import './passwordchange.css'
+import "./Passwordchange.css"
 import { toast } from 'sonner'
 import { useNavigate } from 'react-router'
 import { sendotpAction, updatepasswordAction, verifyOTPAction } from '../../redux/actions/user'
